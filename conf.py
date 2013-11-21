d86c9f7525fa0c4cc82b4c26667a70afab1228cd
Andreas Politz <politza@hochschule-trier.de>
2013-11-21 22:10:24 +0100

    Added conf.py

---
+++ conf.py
@@ -0,0 +1,6 @@
+#BEG texinfo_documents
+texinfo_documents = [
+    ('contents', 'python', 'Python Documentation', _stdauthor,
+     'Python-%s', 'The Python Documentation', 'Software Development'),
+]
+#END texinfo_documents
